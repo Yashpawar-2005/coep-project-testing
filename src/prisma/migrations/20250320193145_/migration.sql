370acc5444b59912f79384d9b0ae5d9a715d3af3
shreemengshetti2005 <shreemengshetti12@gmail.com>
2025-07-14 15:40:17 +0530

    Update migration.sql

--- src/prisma/migrations/20250320193145_/migration.sql
+++ src/prisma/migrations/20250320193145_/migration.sql
@@ -1,10 +1,4 @@
-/*
-  Warnings:
 
-  - You are about to drop the column `discription` on the `Team` table. All the data in the column will be lost.
-
-*/
--- AlterTable
 ALTER TABLE "Team" DROP COLUMN "discription",
 ADD COLUMN     "description" TEXT;
 
